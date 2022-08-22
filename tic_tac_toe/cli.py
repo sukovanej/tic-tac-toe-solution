ed1d14dec30c71e45614761f0ef875bc88c4680f
Milan Suk <Milansuk@email.cz>
2022-08-22 21:23:16 +0200

    feat: clean up cli

--- tic_tac_toe/cli.py
+++ tic_tac_toe/cli.py
@@ -1,17 +1,10 @@
 from .models import GameRules
-from .print import print_game
-from .find_winner import find_winner, initialize_game, play_move
+from .find_winner import find_winner, initialize_game
 
 TIC_TAC_TOE_RULES = GameRules(rows=3, columns=3, connected_to_win=3)
 
 
 def run():
     game = initialize_game(TIC_TAC_TOE_RULES)
-
-    game = play_move((1, 1), game)
-    game = play_move((0, 0), game)
-    print_game(game)
-    print(game.player_to_move)
-
     result = find_winner(game)
     print(result)
